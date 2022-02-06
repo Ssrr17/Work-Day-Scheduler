0d17638c1d462743e59891d60b52b55f4ca8b08f
Salieu Sarr <95061054+Ssrr17@users.noreply.github.com>
2022-02-06 14:18:34 -0500

    blocks created

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,24 @@
-dayByDay = function(){
-//dynamic day creation
+//get and display the current date
+//create blocks for business hours 
+//get user input info 
+//save info to local storage
+//make time loops for past, present and future
+//colour code time status
+//
+
+
+
+
+
+function getHeaderDate() {
+    var toDay = moment().format('dddd, MMMM Do YYYY');
+    $("#currentDay").text(toDay);
+    console.log(toDay);
 }
- 
+
+getHeaderDate()
+
+
+
+
+
